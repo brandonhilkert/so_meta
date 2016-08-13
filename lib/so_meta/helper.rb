9240ed912a989b5d0c73f436b0a8a35a911dda95
Brandon Hilkert <brandonhilkert@gmail.com>
2016-08-13 14:18:44 -0400

    Allow scope to be set

--- lib/so_meta/helper.rb
+++ lib/so_meta/helper.rb
@@ -2,7 +2,14 @@ module SoMeta
   module Helper
     def so_meta(name, scope = nil)
       interpolation_data = instance_variable_get("@so_meta_#{name}_interpolation") || {}
-      t("so_meta.#{controller_path.gsub(/\//, "_")}.#{action_name}.#{name}", interpolation_data.merge(default: t("so_meta.defaults.#{name}")))
+
+      controller_name = controller_path.gsub(/\//, "_")
+
+      if scope.present?
+        t("so_meta.#{controller_name}.#{action_name}.#{scope}.#{name}",
+          default: t("so_meta.#{controller_name}.#{action_name}.#{name}", interpolation_data.merge(default: t("so_meta.defaults.#{name}")))
+        )
+      end
     end
 
     def so_meta_interpolation(name, hash)
